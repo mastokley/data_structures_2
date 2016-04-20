d8ae8d038786abc04f1b88e4844ca1b5a397f98a
Michael Stokley <michael@michaelstokley.com>
2016-04-20 16:11:37 -0700

    Wrote methods, tests. must fix bin sharing logic.

--- src/test_hash_table.py
+++ src/test_hash_table.py
@@ -1,22 +1,48 @@
 
-# import io
-# import pytest
+import io
+import pytest
 
-# with io.open('../data/american-english-small') as file:
-#     WORDS = file.read()
-
-
-# @pytest.fixture
-# def empty_hash_table(scope='function'):
-#     from hash_table import HashTable
-#     hash_table = HashTable()
+with io.open('../data/american-english-small') as file:
+    WORDS = file.read()
 
 
-# def test_instantiation(hash_table):
-#     """"""
-#     assert False
+@pytest.fixture
+def empty_hash_table(scope='function'):
+    from hash_table import HashTable
+    ht = HashTable()
+    return ht
 
 
-# def test_set():
-#     """"""
+@pytest.fixture
+def full_hash_table(scope='function'):
+    from hash_table import HashTable
+    ht = HashTable()
+    ht.set("apple", 1)
+    ht.set("banana", 2)
+    ht.set("crabapple", 3)
+    ht.set("Abraheem van Helsing", "dog")
+    ht.set("Special Agent Dale Cooper, FBI", "dog")
+    ht.set("Oscar", "cat")
+    return ht
+
+
+def test_size_empty(empty_hash_table):
+    """Assert size defaults to 1024."""
+    assert empty_hash_table._size == 1024
+
+
+def test_size_full(full_hash_table):
+    """Assert size remains 1024."""
+    assert full_hash_table._size == 1024
+
+
+def test_table_instantiation(empty_hash_table):
+    """Assert container instantiating."""
+    expected = [[] for i in range(empty_hash_table._size)]
+    assert empty_hash_table._container == expected
+
+
+def test_get(full_hash_table):
+    assert full_hash_table.get('apple') == 1
+
 
