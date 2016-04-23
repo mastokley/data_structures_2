ce49ba0e3568056707ba1bf518de54722ec8bd0c
ptrompeter <patrick.trompeter@gmail.com>
2016-04-22 19:07:17 -0700

    now it works.

--- src/quicksort.py
+++ src/quicksort.py
@@ -1,4 +1,5 @@
 import numpy
+from random import randint
 
 
 def quicksort(iterable):
@@ -11,8 +12,8 @@ def quicksort(iterable):
         middle = iterable[len(iterable) // 2]
         pivot = numpy.median([first, last, middle])
         if len(iterable) == 2:
-            left = [max(iterable)]
-            right = [min(iterable)]
+            left = [min(iterable)]
+            right = [max(iterable)]
         else:
             left, right = [], []
             for item in iterable:
@@ -21,3 +22,16 @@ def quicksort(iterable):
             yield element
         for element in quicksort(right):
             yield element
+
+if __name__ == '__main__':
+    from timeit import Timer
+    input = [randint(0, 1000000) for i in range(500)]
+    t = Timer(lambda: quicksort(input))
+    print("This quicksort has time complexity average complexity O(n log n) and worst case complexity O(n^2)")
+    print("running a 500 item list 500 times")
+    print(t.timeit(number=500))
+    print("running a 50 item list 500 times")
+    input = [randint(0, 1000000) for i in range(50)]
+    t = Timer(lambda: quicksort(input))
+    print(t.timeit(number=500))
+    """print("this is where AJ's crummy solution starts")"""
